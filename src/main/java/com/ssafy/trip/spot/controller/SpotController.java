ffaa6859a5b39761a2274cf9d3e40957d39cc703
jj <shameless8@naver.com>
2024-11-04 17:17:37 +0900

    feat : 예외 처리 환경 설정

--- src/main/java/com/ssafy/trip/spot/controller/SpotController.java
+++ src/main/java/com/ssafy/trip/spot/controller/SpotController.java
@@ -1,5 +1,16 @@
 package com.ssafy.trip.spot.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.trip.spot.dto.SidoDto;
+
+@RestController
+@RequestMapping("/spot")
 public class SpotController {
 
 }
