35d01c86c406aa6f6001947b39fa4aa74dc56f2c
jj <shameless8@naver.com>
2024-11-05 10:16:59 +0900

    feat : 시/도 리스트 가져오기

--- src/main/java/com/ssafy/trip/spot/controller/SpotController.java
+++ src/main/java/com/ssafy/trip/spot/controller/SpotController.java
@@ -1,5 +1,34 @@
 package com.ssafy.trip.spot.controller;
 
-public class SpotController {
+import java.util.List;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.trip.spot.dto.SidoDto;
+import com.ssafy.trip.spot.service.SpotService;
+
+@RestController
+@RequestMapping("/spot")
+public class SpotController {
+	@Autowired
+	private SpotService spotService;
+	
+	public SpotController(SpotService spotService) {
+		this.spotService = spotService;
+	}
+	
+	@GetMapping("/sidos")
+	public ResponseEntity<?> selectAllSidos(){
+		List<SidoDto> sidoList = spotService.selectSidoList();
+		if(sidoList==null || sidoList.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(sidoList, HttpStatus.OK);
+	}
 }
--- src/main/java/com/ssafy/trip/spot/mapper/SpotMapper.java
+++ src/main/java/com/ssafy/trip/spot/mapper/SpotMapper.java
@@ -3,6 +3,7 @@ package com.ssafy.trip.spot.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 
 import com.ssafy.trip.spot.dto.SidoDto;
 
