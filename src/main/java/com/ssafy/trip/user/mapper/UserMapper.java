3856543c9faa1917a431d14dd555d8ce0c5d04c7
jj <shameless8@naver.com>
2024-11-05 10:17:49 +0900

    fix : user 에러 처리

--- src/main/java/com/ssafy/trip/user/mapper/UserMapper.java
+++ src/main/java/com/ssafy/trip/user/mapper/UserMapper.java
@@ -3,8 +3,11 @@ package com.ssafy.trip.user.mapper;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.ssafy.trip.user.dto.UserDto;
 
+@Mapper
 public interface UserMapper {
 	int idCheck(String userId) throws Exception;
 	void joinUser(UserDto userDto) throws Exception;
