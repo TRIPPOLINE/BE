32fec77ff967e03a6bd832380545f11837db9283
jaeik choi <shameless8@naver.com>
2024-11-22 11:36:37 +0900

    fix : 회원 가입, 로그인 요청 허용하도록 설정

--- src/main/java/com/ssafy/trip/global/config/SecurityConfig.java
+++ src/main/java/com/ssafy/trip/global/config/SecurityConfig.java
@@ -24,8 +24,8 @@ public class SecurityConfig {
     private final JwtUtil jwtUtil;
 
     private static final String[] AUTH_WHITELIST = {
-            "auth/**",
-            "/user/join"
+            "api/auth/**",
+            "api/user/join"
     };
 
 
@@ -57,17 +57,17 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화
-                .authorizeHttpRequests(authz -> authz
-                        .anyRequest().permitAll()  // 모든 요청 허용
-                )
-                .httpBasic(Customizer.withDefaults())  // HTTP Basic 인증 설정
-                .formLogin(form -> form.disable())  // 폼 로그인 비활성화
-                .logout(logout -> logout.disable());  // 로그아웃 기능 비활성화
-
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화
+//                .authorizeHttpRequests(authz -> authz
+//                        .anyRequest().permitAll()  // 모든 요청 허용
+//                )
+//                .httpBasic(Customizer.withDefaults())  // HTTP Basic 인증 설정
+//                .formLogin(form -> form.disable())  // 폼 로그인 비활성화
+//                .logout(logout -> logout.disable());  // 로그아웃 기능 비활성화
+//
+//        return http.build();
+//    }
 }
